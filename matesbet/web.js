9ef5a68e31f4e797674366c333b6caae10ddbfef
David Courtney <dsbcourtney@gmail.com>
2014-07-23 16:19:18 +0100

    Bootstrap module

--- matesbet/web.js
+++ matesbet/web.js
@@ -3,11 +3,17 @@ var express = require("express");
 var logfmt = require("logfmt");
 var jade = require("jade");
 var app = express();
+app.use(app.router);
+app.set('views', __dirname + '/assets/jade');
+app.set('view_engine', 'jade');
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+	res.render('template',
+		{title:'Home'}
+		);
+  //res.send('Hello World!');
 });
 
 var port = Number(process.env.PORT || 5000);
